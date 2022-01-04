8cf5e64f6d80be8e93e640fd38235afae511c2c8
Sebastian <sep.gil@gmail.com>
2022-01-04 22:14:23 +0100

    Implement rest of the input.

--- version3/portable-synth/include/Input.h
+++ version3/portable-synth/include/Input.h
@@ -43,17 +43,19 @@ public:
         KEYBOARD_LAST = KEYBOARD_B2,
     };
 
-
-
 private:
     void readData();
 
     void updateKeyboard(KEYBOARD_BUTTONS button, bool pressed);
     void updatePad(int pad, bool pressed);
     void updateMenu(MENU_BUTTONS button, bool pressed);
+    void updateUtility(int key, bool pressed);
+    void updateEncoderButton(int encoder, bool pressed);
+    void updateEncoder(int encoder, int pos, bool moved_left);
 
     byte buttonStates[8] = {0,0,0,0,0,0,0,0}; 
     byte lastButtonStates[8] = {0,0,0,0,0,0,0,0};
+    int encoders[4] = {0,0,0,0};
 
     const byte MENU_BIT_MAPPIN[3] = {5, 4, 6};
 
@@ -91,5 +93,23 @@ private:
         6, 7, 0,
 // SW:  22 23 24
         5, 6, 7,
+    };
+
+    const byte UTILITY_BYTE_MAPPING[8] = {
+// SW:  4  5  6  7
+        0, 0, 0, 0,
+// SW:  11 12 13 14
+        1, 1, 1, 1,
+    };
+
+    const byte UTILITY_BIT_MAPPING[8] = {
+// SW:  4  5  6  7
+        3, 4, 5, 6,
+// SW:  11 12 13 14
+        2, 3, 4, 5,
+    };
+
+    const byte ENCODER_BIT_MAPPING[4] = {
+        0, 1, 2, 3,
     };
 }; 
--- version3/portable-synth/src/input.cpp
+++ version3/portable-synth/src/input.cpp
@@ -32,11 +32,48 @@ void Input::update()
     // Update pads.
     for (int i = 0; i < 12; i++)
     {
-        if (bitRead(buttonStates[PAD_BYTE_MAPPING[i]], PAD_BIT_MAPPING[i]) != bitRead(lastButtonStates[PAD_BYTE_MAPPING[i]], PAD_BIT_MAPPING[i])) {
+        if (bitRead(buttonStates[PAD_BYTE_MAPPING[i]], PAD_BIT_MAPPING[i]) != bitRead(lastButtonStates[PAD_BYTE_MAPPING[i]], PAD_BIT_MAPPING[i]))
+        {
             updatePad(i, bitRead(buttonStates[PAD_BYTE_MAPPING[i]], PAD_BIT_MAPPING[i]));
         }
     }
+
+    // Update utility buttons.
+    for (int i = 0; i < 8; i++)
+    {
+        if (bitRead(buttonStates[UTILITY_BYTE_MAPPING[i]], UTILITY_BIT_MAPPING[i]) != bitRead(lastButtonStates[UTILITY_BYTE_MAPPING[i]], UTILITY_BIT_MAPPING[i]))
+        {
+            updateUtility(i, bitRead(buttonStates[UTILITY_BYTE_MAPPING[i]], UTILITY_BIT_MAPPING[i]));
+        }
+    }
+
+    // Update encoder buttons.
+    for (int i = 0; i < 4; i++)
+    {
+        if (bitRead(buttonStates[6], ENCODER_BIT_MAPPING[i]) != bitRead(lastButtonStates[6], ENCODER_BIT_MAPPING[i]))
+        {
+            updateEncoderButton(i, bitRead(buttonStates[6], ENCODER_BIT_MAPPING[i]));
+        }
+    }
+
+    // Update encoders.
+    if (lastButtonStates[7] != buttonStates[7]) {
+        for (int i = 0; i < 4; i++){
+            byte mask = B00000011;
+            mask = mask << i*2;
+            byte enc_val = buttonStates[7] & mask;
+            enc_val = enc_val >> i*2;
+            if (enc_val == 2){
+                encoders[i]--;
+            }
+            else if (enc_val == 1){
+                encoders[i]++;
+            }
+            updateEncoder(i, encoders[i], enc_val == 2);
+        }
+    }
 }
+
 
 void Input::readData()
 {
@@ -70,29 +107,57 @@ void Input::updateMenu(MENU_BUTTONS button, bool pressed)
     {
     case Input::MENU_MAIN:
         Serial.print("Main menu: ");
-        Serial.println(pressed, HEX);
+        Serial.println(pressed, DEC);
         break;
     case Input::MENU_LEFT:
         Serial.print("Left menu: ");
-        Serial.println(pressed, HEX);
+        Serial.println(pressed, DEC);
         break;
     case Input::MENU_RIGHT:
         Serial.print("Right menu: ");
-        Serial.println(pressed, HEX);
+        Serial.println(pressed, DEC);
         break;
     }
 }
 
-void Input::updateKeyboard(KEYBOARD_BUTTONS button, bool pressed) {
-    if (pressed) {
+void Input::updateKeyboard(KEYBOARD_BUTTONS button, bool pressed)
+{
+    if (pressed)
+    {
         Serial.print("Keyboard pressed ");
         Serial.println(button, DEC);
     }
 }
 
-void Input::updatePad(int pad, bool pressed) {
-    if (pressed) {
+void Input::updatePad(int pad, bool pressed)
+{
+    if (pressed)
+    {
         Serial.print("Pad pressed ");
         Serial.println(pad, DEC);
     }
+}
+
+void Input::updateUtility(int key, bool pressed)
+{
+    if (pressed)
+    {
+        Serial.print("Utility pressed ");
+        Serial.println(key, DEC);
+    }
+}
+
+void Input::updateEncoderButton(int encoder, bool pressed) {
+    if (pressed)
+    {
+        Serial.print("Encoder pressed ");
+        Serial.println(encoder, DEC);
+    }
+}
+
+void Input::updateEncoder(int encoder, int pos, bool moved_left) {
+    Serial.print("Encoder ");
+    Serial.print(encoder, DEC);
+    Serial.print(" moved to pos ");
+    Serial.println(pos, DEC);
 }
